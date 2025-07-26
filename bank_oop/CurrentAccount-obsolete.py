c548ebfcc111d67d08f142b6d4571f93f008c999
Samyak-Roy <112627144+Samyak-Roy@users.noreply.github.com>
2025-07-26 21:26:11 +0530

    added superclass and subclass using inheritance to simplify code

--- bank_oop/CurrentAccount-obsolete.py
+++ bank_oop/CurrentAccount-obsolete.py
@@ -1,12 +1,13 @@
 
 from datetime import datetime
-import BaseAccount
 
-class CurrentAccount(BaseAccount):
+class CurrentAccount:
     def __init__(self, account_number, customer):
-        super().__init__(account_number, customer)
-        self.account_type = 'Current Account'
+        self.account_number = account_number
+        self.balance = 0
         self.overdraft_limit = 10001
+        self.customer = customer
+        self.transactions = []
         
     def withdraw(self,amount):
       # Check if the amount is positive
@@ -21,8 +22,27 @@ class CurrentAccount(BaseAccount):
           self.transactions.append(debit_tx)
         else:
           print('Insufficient balance. Add funds or check overdraft limit.')
+    
+    def deposit(self,amount):
+      if amount <= 0:
+        print('Invalid amount. Please enter a positive value.')
+      else:
+        self.balance += amount
+        print(f'{amount} has been deposited.')
+        print(f'Your current balance is {self.balance}')
+        now = datetime.now()
+        credit_tx = (now, 'credit', amount, 'balance', self.balance)
+        self.transactions.append(credit_tx)
 
     def display_account_details(self):
       print(f'Account Number: {self.account_number}')
       print(f'Account Type: {self.account_type}')
-      print(f'Balance: {self.balance}')        
+      print(f'Balance: {self.balance}') 
+
+    def transaction_log(self):
+      for tx in self.transactions:
+        print(tx)
+    
+
+    
+        
--- bank_oop/SavingsAccount.py
+++ bank_oop/SavingsAccount.py
@@ -1,47 +1,12 @@
-
 from datetime import datetime
+import BaseAccount
 
-class SavingsAccount:
-    def __init__(self, account_number,customer):
-        self.account_number = account_number
-        self.balance = 0
-        self.customer = customer
-        self.transactions = []
-    
-    def withdraw(self,amount):
-      if amount > 0:
-        if self.balance > 0 and self.balance >= amount:
-          self.balance -= amount
-          print(f'{amount} has been deducted.')
-          print(f'Your current balance is {self.balance}')
-          now = datetime.now()
-          debit_tx = (now, 'debit', amount, 'balance', self.balance)
-          self.transactions.append(debit_tx)
-        else:
-          print('Insufficient balance. Add funds.')
-      else:
-        print('Invalid amount. Please enter a positive value.')
-    
-    def deposit(self,amount):
-      if amount <= 0:
-        print('Invalid amount. Please enter a positive value.')
-      else:
-        self.balance += amount
-        print(f'{amount} has been deposited.')
-        print(f'Your current balance is {self.balance}')
-        now = datetime.now()
-        credit_tx = (now, 'credit', amount, 'balance', self.balance)
-        self.transactions.append(credit_tx)
-
+class SavingsAccount(BaseAccount):
+    def __init__(self,account_number,customer):
+        super().__init__(account_number, customer)
+        self.account_type = 'Savings Account'
+      
     def display_account_details(self):
-      print(f'Account Number: {self.account_number}')
-      print(f'Account Type: Savings Account')
-      print(f'Balance: {self.balance}') 
-
-    def transaction_log(self):
-      for tx in self.transactions:
-        print(tx)
-    
-
-    
-        
+        print(f'Account Number: {self.account_number}')
+        print(f'Account Type: {self.account_type}')
+        print(f'Balance: {self.balance}')
